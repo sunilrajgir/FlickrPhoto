ca568a5ec64e9130a62ac3e5717d1858e4c7113a
Sunil-Kumar1 <Sunil.Kumar1@timesinternet.in>
2019-12-08 18:47:29 +0530

    added image model

---
+++ FlickrPhoto/FlickrSearchPhoto/Models/FlickrURLs.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrURLs.swift
+//  FlickrPhoto
+//
+//  Created by sunil.kumar1 on 12/8/19.
+//  Copyright © 2019 sunil.kumar1. All rights reserved.
+//
+
+import UIKit
+
+struct FlickrURLs: Codable {
+    let id : String
+    let owner: String
+    let secret: String
+    let server: String
+    let farm: Int
+}
